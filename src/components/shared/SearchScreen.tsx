a0dd1bf71d87908de99d07f411d4e7ba4130fcbd
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-16 10:59:46 +0700

    Search screen 15% progress

---
+++ src/components/shared/SearchScreen.tsx
@@ -0,0 +1,44 @@
+import Header from './Header';
+
+const SearchScreen = () => {
+  return (
+    <div
+      className="modal fade"
+      id="searchModal"
+      aria-labelledby="searchModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog m-0">
+        <div className="modal-content min-vh-100 min-vw-100 border-0 rounded-0">
+          <Header />
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="searchModalLabel">
+              Modal title
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchScreen;
