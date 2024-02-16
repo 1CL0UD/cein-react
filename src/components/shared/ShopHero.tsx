98f63612a2518dd6e15414017c24ed06250dd9b6
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-16 21:03:38 +0700

    Made hero section for shop page

---
+++ src/components/shared/ShopHero.tsx
@@ -0,0 +1,21 @@
+const ShopHero = () => {
+  return (
+    <div className="container-fluid shop-hero px-0 py-5">
+      <div className="container-fluid px-5 py-4">
+        <div className="row my-auto">
+          <div className="col-4">
+            <h1 className="fs-1">Skin Care</h1>
+            <p className="fs-6">
+              The skin is constantly changing, influenced by the environment,
+              lifestyle, and diet. Our range is crafted with this in
+              consideration, addressing various preferences and needs to help
+              you achieve optimal skin health.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopHero;
