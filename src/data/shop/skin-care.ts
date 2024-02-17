b80d35bb3b172821f80abb4fcf2096ce003f8f06
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-17 21:55:16 +0700

    mock data for skin care products

---
+++ src/data/shop/skin-care.ts
@@ -0,0 +1,184 @@
+export default {
+  data: [
+    {
+      id: 1039,
+      brand: 'w3llpeople',
+      name: 'Realist Invisible Setting Powder',
+      capacity: '20 ml',
+      price: 10.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'Keep it real with a polished, soft-focus look using this superfine, loose mineral powder. The healing properties of organic aloe help soothe and calm stressed out skin. Instantly minimize pores, soften imperfections, and creates a matte finish with superfine powder. Use by itself for an air-brushed look, or to set foundation for ultra-long wear. Set makeup or by itself for matte air-brushed effect. Absorbs excess oil while softening fine lines. Organic aloe creates radiant, rejuvenated skin. Hypoallergenic, Chemical Free, Cruelty Free & Noncomedogenic. EWG Verified for non-toxic health and safety. Free From: Parabens, Gluten, GMO, Phthalates, Sulfates, Fragrance, Pheonoxyethanol, Dimethicone, PEG, Propylene Glycol, Butylene Glycol, Lead, Mineral Oil, Talc',
+      category: 'powder',
+      image_link: 'https://static.ewg.org/skindeep_images/6528/652831.jpg',
+    },
+    {
+      id: 1040,
+      brand: 'Maybelline',
+      name: 'Fit Me Loose Finishing Powder',
+      capacity: '200 ml',
+      price: 7.99,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'This ultra-fine loose powder sets makeup for a long-lasting, shine-free finish. It absorbs oil and controls shine without looking cakey or drying out the skin. Suitable for all skin types.',
+      category: 'powder',
+      image_link:
+        'https://down-id.img.susercontent.com/file/0717883768ef7742b393a26f8b4c1179',
+    },
+    {
+      id: 1041,
+      brand: 'Laura Mercier',
+      name: 'Translucent Loose Setting Powder',
+      capacity: '10 ml',
+      price: 38.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This cult-favorite powder sets makeup for a flawless, invisible finish. It's finely milled and weightless, so it won't settle into pores or lines. Suitable for all skin types.",
+      category: 'powder',
+      image_link:
+        'https://s3-ap-southeast-1.amazonaws.com/soc-uploader/images/NUxXVrID1GVJlJKVzcbX63W1axTfFt_1500289249.jpg',
+    },
+    {
+      id: 1042,
+      brand: 'NARS',
+      name: 'Light Reflecting Loose Setting Powder',
+      capacity: '25 ml',
+      price: 38.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This powder sets makeup and blurs imperfections while leaving a radiant, lit-from-within finish. It's micro-fine and weightless, so it won't settle into pores or lines. Suitable for all skin types.",
+      category: 'powder',
+      image_link:
+        'https://www.sephora.com/productimages/sku/s2514826-main-zoom.jpg?imwidth=315',
+    },
+    {
+      id: 1043,
+      brand: 'Fenty Beauty',
+      name: "Pro Filt'r Instant Retouch Setting Powder",
+      capacity: '23 ml',
+      price: 34.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This powder sets makeup, mattifies skin, and blurs imperfections with a lightweight, buildable formula. It's available in a wide range of shades to suit all skin tones. Suitable for all skin types.",
+      category: 'powder',
+      image_link:
+        'https://images-cdn.ubuy.co.id/658c402d5fc95b23a57f8b65-fenty-beauty-pro-filt-39-r-instant.jpg',
+    },
+    {
+      id: 1044,
+      brand: 'Too Faced',
+      name: 'Born This Way Ethereal Setting Powder',
+      capacity: '80 ml',
+      price: 42.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This powder sets makeup with a lightweight, breathable formula that won't cake or settle into pores. It hydrates skin and blurs imperfections for a flawless, natural finish. Suitable for all skin types.",
+      category: 'powder',
+      image_link:
+        'https://lookincredible.co.uk/cdn/shop/products/hnjm_grande.jpg?v=1685015740',
+    },
+    {
+      id: 1045,
+      brand: 'MILK MAKEUP',
+      name: 'Blur + Set Matte Loose Setting Powder',
+      capacity: '90 ml',
+      price: 32.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'This vegan and cruelty-free powder sets makeup and mattifies skin with a lightweight, breathable formula. It blurs imperfections and controls shine without looking cakey. Suitable for all skin types.',
+      category: 'powder',
+      image_link:
+        'https://www.sephora.com/productimages/sku/s2702751-main-hero.jpg',
+    },
+    {
+      id: 1046,
+      brand: 'Huda Beauty',
+      name: 'Easy Bake Loose Setting Powder',
+      capacity: '320 ml',
+      price: 32.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This powder sets makeup for a long-lasting, flawless finish. It's finely milled and buildable, so you can create a natural or full-coverage look. Suitable for all skin types.",
+      category: 'powder',
+      image_link:
+        'https://images-cdn.ubuy.co.id/6350dccd9d905378bb2af058-new-huda-beauty-easy-bake-loose-setting.jpg',
+    },
+    {
+      id: 1047,
+      brand: 'Charlotte Tilbury',
+      name: 'Airbrush Flawless Finish Powder',
+      capacity: '239 ml',
+      price: 52.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This powder sets makeup for a flawless, airbrushed finish. It's finely milled and weightless, so it won't settle into pores or lines. Available in a wide range of shades to suit all skin tones. Suitable for all skin types.",
+      category: 'powder',
+      image_link: 'https://s.cdnsbn.com/images/products/l/25833590602-1.jpg',
+    },
+    {
+      id: 1048,
+      brand: 'Rare Beauty',
+      name: 'Always An Optimist Illuminating Setting Powder',
+      capacity: '287 ml',
+      price: 28.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'This weightless powder sets makeup with a soft-focus, radiant finish. It blurs imperfections and leaves skin looking naturally luminous. Suitable for all skin types.',
+      category: 'powder',
+      image_link:
+        'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/9/28/425d25dd-0688-446c-b82f-459455216f41.jpg',
+    },
+    {
+      id: 1049,
+      brand: 'KVD Vegan Beauty',
+      name: 'Lock-It Setting Powder Powder',
+      capacity: '102 ml',
+      price: 28.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'This ultra-fine, loose powder sets makeup for a long-lasting, matte finish. It controls shine and absorbs excess oil without looking cakey. Suitable for all skin types.',
+      category: 'powder',
+      image_link:
+        'https://cdn.shopify.com/s/files/1/0613/8429/4622/files/ScreenShot2023-06-12at1.47.04PM_91fcc33c-988f-49f9-85a5-fdae7c9fbe7a_1500x.png?v=1698802538',
+    },
+    {
+      id: 1050,
+      brand: 'Urban Decay',
+      name: 'All Nighter Setting Spray',
+      capacity: '478 ml',
+      price: 33.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        "This weightless setting spray keeps makeup in place for up to 16 hours. It's waterproof, sweatproof, and transfer-proof. Suitable for all skin types.",
+      category: 'setting spray',
+      image_link:
+        'https://www.urbandecay.com/dw/image/v2/AAFM_PRD/on/demandware.static/-/Sites-urbandecay-master-catalog/default/dw3c37da0d/ProductImages/Eyes/PerversionMascara_Travel/3605970888459_perversiontravel.jpg?sw=362&sfrm=jpg&q=70',
+    },
+    {
+      id: 1051,
+      brand: 'MAC Cosmetics',
+      name: 'Prep + Prime Fix+',
+      capacity: '134 ml',
+      price: 27.0,
+      price_sign: '$',
+      currency: 'USD',
+      description:
+        'This multi-tasking spray primes, sets makeup, and refreshes the skin. It hydrates, soothes, and provides a radiant finish. Suitable for all skin types.',
+      category: 'setting spray',
+      image_link:
+        'https://sdcdn.io/mac/us/mac_sku_SNH801_1x1_0.png?width=1080&height=1080',
+    },
+  ],
+};
