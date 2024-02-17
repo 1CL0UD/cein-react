582a37791f10b3f70d5f50317d2ded416764d259
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-18 00:34:03 +0700

    Created Product Page Layout

--- src/pages/products/LayoutProducts.tsx
+++ src/pages/products/LayoutProducts.tsx
@@ -1,5 +1,12 @@
-const LayoutProducts = () => {
-  return <div>LayoutProducts</div>;
+import { ReactNode } from 'react';
+import RootLayout from '../RootLayout';
+
+interface Props {
+  children: ReactNode;
+}
+
+const LayoutProducts = ({ children }: Props) => {
+  return <RootLayout>{children}</RootLayout>;
 };
 
 export default LayoutProducts;
