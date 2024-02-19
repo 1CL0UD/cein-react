3795f0c74eaef11baf499d6fc87bc8de2c23c848
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-19 14:53:02 +0700

    adjusted the page for smaller screens

--- src/pages/shop/skin-care/SkinCarePage.tsx
+++ src/pages/shop/skin-care/SkinCarePage.tsx
@@ -1,8 +1,12 @@
 import ShopProducts from '../../../components/shared/ShopProducts';
+import JournalCard from '../../../components/ui/JournalCard';
 import skinCare from '../../../data/shop/skin-care';
 import LayoutShop from '../LayoutShop';
 
 const SkinCarePage = () => {
+  const journal = Array.from({ length: 7 }, (_, index) => (
+    <JournalCard key={index} />
+  ));
   return (
     <LayoutShop>
       <>
@@ -11,6 +15,14 @@ const SkinCarePage = () => {
           <h4 className="fs-4">Essentials For Every Skincare</h4>
           <ShopProducts products={skinCare.data} />
         </div>
+        <section className="container-fluid carousel">
+          <p>Insights into Health and Well-being</p>
+          <h1>Our Journal</h1>
+          <div className="media-scroller snaps-inline">{journal}</div>
+          <a href="/blog" className="btn mt-4">
+            All Blog Posts <i className="bi bi-chevron-right"></i>
+          </a>
+        </section>
       </>
     </LayoutShop>
   );
