439b9d8b19895c43300e52cf6c03a8c31db78f33
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-19 14:55:54 +0700

    Added route to cart page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import SkinCarePage from './pages/shop/skin-care/SkinCarePage.tsx';
 import ProductPage from './pages/products/ProductPage.tsx';
+import CartPage from './pages/cart/CartPage.tsx';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: '/products/product-1',
     element: <ProductPage />,
+  },
+  {
+    path: '/cart',
+    element: <CartPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
