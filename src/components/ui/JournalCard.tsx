2e4e5d95709859b4778f1657ab595a4ed9720faa
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-19 14:44:16 +0700

    Made journal card component

---
+++ src/components/ui/JournalCard.tsx
@@ -0,0 +1,15 @@
+const JournalCard = () => {
+  return (
+    <div className="media-element journal">
+      <img src="/img/journal/journal-1.png" alt="product-2" />
+      <p>SKIN CONCIERGE</p>
+      <h4>Distinguishing Mineral Sunscreens from Chemical Sunscreens</h4>
+      <button className="read-more-btn">
+        Read More
+        <i className="bi bi-chevron-right"></i>
+      </button>
+    </div>
+  );
+};
+
+export default JournalCard;
