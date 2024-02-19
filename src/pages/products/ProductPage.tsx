14310343defce4480d2cc23201d6ccbd43acad3c
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-19 09:14:27 +0700

    90% product page

--- src/pages/products/ProductPage.tsx
+++ src/pages/products/ProductPage.tsx
@@ -1,21 +1,25 @@
 import Accordion from '../../components/ui/Accordion';
 import AddToCartButton from '../../components/ui/AddToCartButton';
+import Comments from '../../components/ui/Comments';
+import ProductCard from '../../components/ui/ProductCard';
 import ProductDescription from '../../components/ui/ProductDescription';
 import LayoutProducts from './LayoutProducts';
+import skinCare from '../../data/shop/skin-care';
 
 const ProductPage = () => {
+  const products = skinCare.data;
   return (
     <LayoutProducts>
-      <div className="container-fluid pe-5">
+      <div className="container-fluid">
         <div className="row min-vh-100">
-          <div className="col-8 ps-0">
+          <div className="col-md-8 row px-0">
             <img
               src="/img/products/product-1.png"
-              className="object-fit-cover w-100"
+              className="object-fit-contain w-100 p-0"
               alt="Product Name"
             />
           </div>
-          <div className="col-4 my-auto">
+          <div className="col-md-4 p-4 row m-auto">
             <div className="d-flex flex-row">
               <p className="fs-6">Skin Care</p>
               <i className="bi bi-dot"></i>
@@ -46,20 +50,50 @@ const ProductPage = () => {
           <p className="fs-4 fw-semibold">Gentle Care for Delicate Skin</p>
         </div>
       </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-6 bg-snow-drift p-0">
+      <div className="container-fluid p-0">
+        <div className="row m-0">
+          <div className="col-md-6 row bg-snow-drift">
             <Accordion />
           </div>
-          <div className="col-6 p-0">
+          <div className="col-md-6 row m-0 p-0">
             <img
               src="/img/products/product-acc.png"
-              className="w-100 object-fit-cover"
+              className="object-fit-cover w-100 p-0"
               alt="Product Accordion"
             />
           </div>
         </div>
       </div>
+      <div className="container-fluid py-5">
+        <div className="row">
+          <div className="col-md-6">Reviews</div>
+          <div className="col-md-6">
+            <Comments />
+            <Comments />
+            <Comments />
+          </div>
+        </div>
+      </div>
+      <section className="container-fluid carousel">
+        <p className="fs-6">Suggested</p>
+        <h1 className="fs-1">Combine with...</h1>
+        <p className="fs-6">You might also like the following products</p>
+        <div className="media-scroller snaps-inline">
+          {products.map((item) => (
+            <ProductCard
+              id={item.id}
+              name={item.name}
+              capacity={item.capacity}
+              price={item.price}
+              price_sign={item.price_sign}
+              image_link={item.image_link}
+            />
+          ))}
+        </div>
+        <a href="/shop/skin-care" className="btn mt-4">
+          All Products <i className="bi bi-chevron-right"></i>
+        </a>
+      </section>
     </LayoutProducts>
   );
 };
