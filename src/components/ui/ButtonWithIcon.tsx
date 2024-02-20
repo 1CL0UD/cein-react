61e5ee7333980084214fd9d47a9b5fd74b0a1c5b
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-20 14:17:39 +0700

    added href to button prop

--- src/components/ui/ButtonWithIcon.tsx
+++ src/components/ui/ButtonWithIcon.tsx
@@ -1,19 +1,21 @@
 interface Props {
+  href: string;
   dark: boolean;
   children: string;
 }
-const ButtonWithIcon = ({ children, dark }: Props) => {
+const ButtonWithIcon = ({ children, dark, href }: Props) => {
   const darkStyle = dark ? 'text-light border-white' : 'text-dark border-black';
 
   return (
-    <button
+    <a
+      href={href}
       className={`btn btn-outline-${
         dark ? 'dark' : 'light'
       } ${darkStyle} py-3 px-4 rounded-0 d-flex justify-content-between`}
     >
       {children}
       <i className="bi bi-chevron-right"></i>
-    </button>
+    </a>
   );
 };
 
