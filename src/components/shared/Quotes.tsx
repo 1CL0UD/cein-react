989b8c4d40c920e0cf0c8a7a550e7f570fc1e2b9
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-22 00:47:17 +0700

    added small subtext prop

--- src/components/shared/Quotes.tsx
+++ src/components/shared/Quotes.tsx
@@ -1,16 +1,23 @@
 interface Props {
   children: string;
   subtext: string;
+  smallSubText: boolean;
   bg: boolean;
 }
 
-const Quotes = ({ children, subtext, bg }: Props) => {
+const Quotes = ({ children, subtext, bg, smallSubText }: Props) => {
   return (
     <section
-      className={`row d-flex text-center ${bg ? 'bg-snow-drift' : ''} quotes`}
+      className={`row d-flex text-center ${bg ? 'bg-snow-drift' : ''}  quotes`}
     >
       <h2 className="fs-2 mb-4">{children}</h2>
-      <h4 className="fs-4 fw-bold text-uppercase">{subtext}</h4>
+      <h4
+        className={` ${
+          smallSubText ? 'fs-5 fw-lighter' : 'fs-4 fw-bold text-uppercase'
+        } `}
+      >
+        {subtext}
+      </h4>
     </section>
   );
 };
