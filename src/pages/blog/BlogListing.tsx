fc9f8d7db0192fcec9b38c5ae0fc78d395fab414
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-21 18:47:38 +0700

    Fixed layout for smaller screens

--- src/pages/blog/BlogListing.tsx
+++ src/pages/blog/BlogListing.tsx
@@ -11,12 +11,12 @@ const BlogListing = () => {
     <RootLayout>
       <HeroWindow textStart={false} />
       <section className="container-fluid carousel">
-        <p>Insights into Health and Well-being</p>
-        <h1>Our Journal</h1>
+        <p className="fs-6">Insights into Health and Well-being</p>
+        <h1 className="fs-1 mb-5">Our Journal</h1>
         <div className="media-scroller snaps-inline">{journal}</div>
       </section>
       <section className="container-fluid">
-        <div className="row">
+        <div className="row mb-5">
           <div className="col-md-6 about-us-img d-flex flex-column justify-content-center align-items-center">
             <img
               src="/img/blog/blog-listing-1.png"
@@ -24,7 +24,7 @@ const BlogListing = () => {
               className="w-100 h-100"
             />
           </div>
-          <div className="col-md-6 d-flex flex-column justify-content-center pe-5">
+          <div className="col-md-6 d-flex flex-column justify-content-center pe-md-5 ps-md-0 px-4">
             <p className="fs-6 text-secondary text-uppercase mb-0">
               Knowledge Treasures
             </p>
@@ -42,7 +42,48 @@ const BlogListing = () => {
               consultants are eager to introduce you to the Aesop range and
               assist with your choices.
             </p>
-            <div className="col-3">
+            <div className="col-md-3 col-12">
+              <ButtonWithIcon
+                href={'/journal'}
+                dark={false}
+                border={false}
+                children={'Read More'}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container-fluid carousel">
+        <div className="media-scroller snaps-inline">{journal}</div>
+      </section>
+      <section className="container-fluid">
+        <div className="row mb-5">
+          <div className="col-md-6 about-us-img d-flex flex-column justify-content-center align-items-center">
+            <img
+              src="/img/blog/blog-listing-2.png"
+              alt="Journal Image"
+              className="w-100 h-100"
+            />
+          </div>
+          <div className="col-md-6 d-flex flex-column justify-content-center pe-md-5 ps-md-0 px-4">
+            <p className="fs-6 text-secondary text-uppercase mb-0">
+              Knowledge Treasures
+            </p>
+            <p className="fs-6 fw-bold">Mastering The Art of Well Aging</p>
+            <p className="fs-6">
+              Our line features meticulous skin, hair, and body care
+              formulations, crafted with both efficacy and sensory delight in
+              focus.
+            </p>
+            <p className="fs-6">
+              We are dedicated to creating top-quality skin, hair, and body care
+              products. We extensively research plant-based and lab-made
+              ingredients, selecting only those with proven safety and
+              effectiveness. At our distinctive stores, knowledgeable
+              consultants are eager to introduce you to the Aesop range and
+              assist with your choices.
+            </p>
+            <div className="col-md-3 col-12">
               <ButtonWithIcon
                 href={'/journal'}
                 dark={false}
