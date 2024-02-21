e41b14bad409f9f9045ed68efa27988dee7a7b55
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-21 18:37:10 +0700

    Adjusted text sizes

--- src/components/ui/JournalCard.tsx
+++ src/components/ui/JournalCard.tsx
@@ -2,8 +2,10 @@ const JournalCard = () => {
   return (
     <div className="media-element journal">
       <img src="/img/journal/journal-1.png" alt="product-2" />
-      <p>SKIN CONCIERGE</p>
-      <h4>Distinguishing Mineral Sunscreens from Chemical Sunscreens</h4>
+      <p className="fs-6 text-uppercase">Skin Concierge</p>
+      <h5 className="fs-5 fw-semibold">
+        Distinguishing Mineral Sunscreens from Chemical Sunscreens
+      </h5>
       <button className="read-more-btn">
         Read More
         <i className="bi bi-chevron-right"></i>
