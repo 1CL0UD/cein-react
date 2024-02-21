b7286dac2669bacf6633d2c8ffc4e9ecba5dcedd
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-21 18:23:21 +0700

    Added hero section

---
+++ src/pages/blog/BlogListing.tsx
@@ -0,0 +1,12 @@
+import HeroWindow from '../../components/shared/HeroWindow';
+import RootLayout from '../RootLayout';
+
+const BlogListing = () => {
+  return (
+    <RootLayout>
+      <HeroWindow textStart={false} />
+    </RootLayout>
+  );
+};
+
+export default BlogListing;
