1caeca60f6ead1d1195de5bc6f68aa1e1f1d3cd5
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-22 01:13:05 +0700

    blog post page done

--- src/pages/blog/BlogPost.tsx
+++ src/pages/blog/BlogPost.tsx
@@ -1,7 +1,11 @@
 import Quotes from '../../components/shared/Quotes';
+import JournalCard from '../../components/ui/JournalCard';
 import RootLayout from '../RootLayout';
 
 const BlogPost = () => {
+  const journal = Array.from({ length: 5 }, (_, index) => (
+    <JournalCard key={index} />
+  ));
   return (
     <RootLayout>
       <section className="container-fluid">
@@ -36,6 +40,34 @@ const BlogPost = () => {
           Countless solutions have been discovered in nature. We simply need to
           observe and tap into its inherent brilliance.
         </Quotes>
+      </section>
+      <section className="container-fluid">
+        <div className="row px-md-4 px-2 mb-5">
+          <div className="col-sm-8 mb-4">
+            <img
+              src="/img/products/product-1.png"
+              alt="Product Image"
+              className="w-100 h-100 object-fit-cover"
+            />
+          </div>
+          <div className="col-sm-4 d-flex flex-column justify-content-between">
+            <img
+              src="/img/products/product-1.png"
+              alt="Product Image"
+              className="w-100 object-fit-cover mb-4"
+            />
+            <img
+              src="/img/products/product-1.png"
+              alt="Product Image"
+              className="w-100 object-fit-cover mb-4"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="container-fluid carousel">
+        <p className="fs-6">Insights into Health and Well-being</p>
+        <h1 className="fs-1 mb-5">Our Journal</h1>
+        <div className="media-scroller snaps-inline">{journal}</div>
       </section>
     </RootLayout>
   );
