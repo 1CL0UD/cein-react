e2d624ca4604c39ed51b818ac4ee83e68a6a8cd6
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-21 18:23:40 +0700

    Added journal section

--- src/pages/blog/BlogListing.tsx
+++ src/pages/blog/BlogListing.tsx
@@ -1,10 +1,19 @@
 import HeroWindow from '../../components/shared/HeroWindow';
+import JournalCard from '../../components/ui/JournalCard';
 import RootLayout from '../RootLayout';
 
 const BlogListing = () => {
+  const journal = Array.from({ length: 5 }, (_, index) => (
+    <JournalCard key={index} />
+  ));
   return (
     <RootLayout>
       <HeroWindow textStart={false} />
+      <section className="container-fluid carousel">
+        <p>Insights into Health and Well-being</p>
+        <h1>Our Journal</h1>
+        <div className="media-scroller snaps-inline">{journal}</div>
+      </section>
     </RootLayout>
   );
 };
