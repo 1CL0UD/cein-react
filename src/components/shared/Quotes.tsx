5a6f7dad09e33d457eda97d9bd45ec8076ab55f5
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-22 00:35:31 +0700

    added bg prop

--- src/components/shared/Quotes.tsx
+++ src/components/shared/Quotes.tsx
@@ -1,11 +1,14 @@
 interface Props {
   children: string;
   subtext: string;
+  bg: boolean;
 }
 
-const Quotes = ({ children, subtext }: Props) => {
+const Quotes = ({ children, subtext, bg }: Props) => {
   return (
-    <section className="row d-flex text-center quotes">
+    <section
+      className={`row d-flex text-center ${bg ? 'bg-snow-drift' : ''} quotes`}
+    >
       <h2 className="fs-2 mb-4">{children}</h2>
       <h4 className="fs-4 fw-bold text-uppercase">{subtext}</h4>
     </section>
