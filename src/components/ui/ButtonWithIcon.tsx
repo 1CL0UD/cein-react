28d82845004de9da5261ea44e34114207743b2a0
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-02-21 18:29:21 +0700

    Added border property to set border

--- src/components/ui/ButtonWithIcon.tsx
+++ src/components/ui/ButtonWithIcon.tsx
@@ -1,9 +1,10 @@
 interface Props {
   href: string;
   dark: boolean;
+  border: boolean;
   children: string;
 }
-const ButtonWithIcon = ({ children, dark, href }: Props) => {
+const ButtonWithIcon = ({ children, dark, href, border }: Props) => {
   const darkStyle = dark ? 'text-light border-white' : 'text-dark border-black';
 
   return (
@@ -11,7 +12,9 @@ const ButtonWithIcon = ({ children, dark, href }: Props) => {
       href={href}
       className={`btn btn-outline-${
         dark ? 'dark' : 'light'
-      } ${darkStyle} py-3 px-4 rounded-0 d-flex justify-content-between`}
+      } ${darkStyle} py-3 px-4 ${
+        border ? '' : 'border-0 ps-0'
+      } rounded-0 d-flex justify-content-between`}
     >
       {children}
       <i className="bi bi-chevron-right"></i>
